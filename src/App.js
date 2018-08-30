0c70f25f15457f7c1569bf6b0386c1f25dcd673c
Zuhra <zuhrasofyan@gmail.com>
2018-08-31 01:29:59 +0700

    try using a react-geo component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import './react-geo.css';
+
+import {
+  SimpleButton
+} from '@terrestris/react-geo';
+
 
 class App extends Component {
   render() {
@@ -11,7 +18,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <SimpleButton
+            onClick={() => {alert('Hello World!');}}
+          >
+            Hello world!
+          </SimpleButton>
         </p>
       </div>
     );
