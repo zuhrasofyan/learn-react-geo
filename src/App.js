f9bca19032db97e8690a4a5eeedf4931cbaf5c7c
Zuhra <zuhrasofyan@gmail.com>
2018-08-31 01:43:17 +0700

    try add map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'ol/ol.css'
+
 import 'antd/dist/antd.css';
 import './react-geo.css';
+
+import './index.css';
+
+import OlMap from 'ol/map';
+import OlView from 'ol/view';
+import OlLayerTile from 'ol/layer/tile';
+import OlSourceOsm from 'ol/source/osm';
 
 import {
   SimpleButton
 } from '@terrestris/react-geo';
+
+import {
+  MapComponent
+} from '@terrestris/react-geo';
+
+const layer = new OlLayerTile({
+  source: new OlSourceOsm()
+});
+
+const center = [ 788453.4890155146, 6573085.729161344 ];
+
+// create a new instance of ol.map in ES6 syntax
+const map = new OlMap({
+  view: new OlView({
+    center: center,
+    zoom: 16,
+  }),
+  layers: [layer]
+});
 
 
 class App extends Component {
@@ -24,6 +52,9 @@ class App extends Component {
             Hello world!
           </SimpleButton>
         </p>
+        <MapComponent 
+          map={map}
+        />
       </div>
     );
   }
