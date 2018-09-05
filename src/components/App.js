87ec44759e499416ece5ebe974e3146a4e55f1d2
Zuhra <zuhrasofyan@gmail.com>
2018-09-06 00:29:59 +0700

    add headernav component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import './App.css';
+
+import Navigation from '../components/Navigation';
 import {Map} from '../components/Map/Map.js';
 
 import {
@@ -11,10 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Navigation />
         <p className="App-intro">
           <SimpleButton
             onClick={() => {alert('Hello World!');}}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
