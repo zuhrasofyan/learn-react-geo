ccac3e77816cfc11f44c516e1d0a9bd7e3ff7708
Zuhra <zuhrasofyan@gmail.com>
2018-09-10 11:16:56 +0700

    move sider to a component that is part of map component

--- src/components/Navigation/HeaderNav.js
+++ src/components/Navigation/HeaderNav.js
@@ -7,21 +7,19 @@ const { Header } = Layout;
 class HeaderNav extends Component {
   render() {
     return(
-
-          <Header className="header">
-            <div className="logo" />
-            <Menu
-              theme="dark"
-              mode="horizontal"
-              defaultSelectedKeys={['2']}
-              style={{ lineHeight: '64px' }}
-            >
-              <Menu.Item key="1">Bagian 1</Menu.Item>
-              <Menu.Item key="2">Bagian 2</Menu.Item>
-              <Menu.Item key="3">Bagian 3</Menu.Item>
-            </Menu>
-          </Header>
-
+      <Header className="header">
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          style={{ lineHeight: '64px' }}
+        >
+          <Menu.Item key="1">Bagian 1</Menu.Item>
+          <Menu.Item key="2">Bagian 2</Menu.Item>
+          <Menu.Item key="3">Bagian 3</Menu.Item>
+        </Menu>
+      </Header>
     );
   }
 }
